0679c69813d6f7e2c1f14d28fe469ccab8cd4328
jxhao <hjx1_3@hotmail.com>
2021-07-09 00:09:46 -0400

    Python Thur

---
+++ PyPoll.py
@@ -0,0 +1,8 @@
+import os
+import csv
+
+#find the file of data to be analyzed
+sourcefile = os.path.join('Resources','election_results.csv')
+
+with open(sourcefile,'r') as election_data:
+    pass
