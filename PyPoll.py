6cb20642fb93896793540ecd15a9b89db6344525
jxhao <jasonhao80s@gmail.com>
2021-07-11 00:36:22 -0400

    7/10

--- PyPoll.py
+++ PyPoll.py
@@ -1,8 +1,21 @@
-import os
+# Dependencies:
 import csv
+import os
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-#find the file of data to be analyzed
-sourcefile = os.path.join('Resources','election_results.csv')
 
-with open(sourcefile,'r') as election_data:
-    pass
+
+total_votes = 0
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
+
+    # Read the header row.
+    headers = next(file_reader)
+    for row in file_reader:
+        total_votes += 1
+print(headers)
